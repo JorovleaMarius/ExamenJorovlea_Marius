da6b3873577ff89233453760f77a96387b0ce10e
JorovleaMarius <159428883+JorovleaMarius@users.noreply.github.com>
2024-04-08 09:39:13 +0300

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,7 @@
+// Afișează un mesaj în consolă
+console.log("Salut din script.js!");
+
+// Obține elementele HTML utilizând metoda document.querySelector și adaugă un eveniment de click
+document.querySelector('button').addEventListener('click', function() {
+    alert('Butonul a fost apăsat!');
+});
